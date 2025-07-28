d4ee74e2e6109a89e5f21314f7967dd22d1e01bc
Alex Borisov <borisovalexdev@gmail.com>
2025-07-28 20:50:10 +0300

    Add and configure React Router

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import './index.css';
 import { App } from './App.tsx';
 import { store } from './app/store.ts';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+   <Provider store={store}>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </Provider>
 );
