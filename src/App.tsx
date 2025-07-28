08e8ffe293b668ced2475f11efae697c02fff9a5
Alex Borisov <borisovalexdev@gmail.com>
2025-07-28 21:07:42 +0300

    Set up routes for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Routes, Route, Navigate } from "react-router";
+import { LoginPage } from "./pages/LoginPage";
+import { DashboardPage } from "./pages/DashboardPage";
+import { UsersPage } from "./pages/UsersPage";
+
+
 export const App = () => {
     return (
-        <>
-            <div></div>
-        </>
+        <Routes>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/dashboard" element={<DashboardPage/>}/>
+            <Route path="/users" element={<UsersPage/>}/>
+            <Route path="*" element={<Navigate to='/dashboard' replace/>}/>
+        </Routes>
     );
 };
