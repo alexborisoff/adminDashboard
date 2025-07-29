2194ea44570f914d88333b17a0fad6770b9fcff2
Alex Borisov <borisovalexdev@gmail.com>
2025-07-29 17:31:03 +0300

    Minor stylish edits

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -15,7 +15,7 @@ export const LoginPage = () => {
       <div className="p-4 text-center">
          <h2 className="text-2xl mb-4">Login Page</h2>
          <button
-            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+            className="px-4 py-2 bg-blue-600 text-white rounded-3xl hover:bg-blue-700 cursor-pointer"
             onClick={handleLogin}
          >
             Login
